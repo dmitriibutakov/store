c559cba4f68e4baccaba016e92437800ea454fe1
dmitryload <dmitryload@yahoo.com>
2022-09-20 01:25:10 +0300

    screen product universal

---
+++ components/productScreen/productScreen.vue
@@ -0,0 +1,79 @@
+<template>
+   <div :class="screen ? 'screen' : 'screen2' ">
+      <h1 class="screen__title">{{title}}</h1>
+      <h2 :class="screen ? 'screen__about' : 'screen__about'">
+         {{about}}
+      </h2>
+      <p class="screen__description">{{description}}</p>
+      <Button :name="'buy'" />
+      <img class="screen__img" :src="image" alt="product">
+   </div>
+</template>
+
+<script>
+export default {
+   props: {
+      title: String,
+      about: String,
+      description: String,
+      image: String,
+      screen: Boolean
+   }
+}
+</script>
+
+<style scoped lang="scss">
+.screen, .screen2 {
+   padding: 40px 5px 0;
+   display: flex;
+   flex-direction: column;
+   justify-content: flex-end;
+   align-items: center;
+   text-align: center;
+   width: calc(100% + 20px);
+   height: 100vh;
+   overflow: hidden;
+   &>*:not(:last-child) {
+      margin-bottom: 13px;
+   }
+
+   .screen__title {
+      font-size: 24px;
+      font-weight: 600;
+   }
+
+   .screen__about,
+   .screen2__about {
+      color: transparent;
+      font-size: 40px;
+      font-weight: 600;
+      background-clip: text;
+   }
+
+   .screen__about {
+      background-image: linear-gradient(180deg, #ffb6ff, #b344ff);
+   }
+
+   .screen2__about {
+      background-image: linear-gradient(90deg, #2ca2b4, #5598de 24%, #7f87ff 45%, #f65aad 76%, #ec3d43);
+   }
+
+   .screen__description {
+      font-size: 21px;
+      margin-bottom: 20px;
+   }
+
+   .screen__img {
+      bottom: 0;
+      width: 100%;
+      max-width: 400px;
+   }
+}
+.screen {
+   color: #f5f5f7;
+   background-color: #000;
+}
+.screen2 {
+   background-color: #fff;
+}
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -3,11 +3,23 @@
     <ul>
       <li><img :src="blocks[0]" alt="block"></li>
     </ul>
-    Here you can to get more information about last products
-    <iphone-screen :title="'iPhone 14 Pro'"
+    <product-screen :title="'iPhone 14 Pro'"
+    :screen="true"
     :about="'Pro.Beyond.'"
     :description="'From $41.62/mo. for 24 mo. or $999 before trade‑in'"
     :image="'images/iphone14Pro.jpg'"/>
+
+    <product-screen :title="'iPhone 14'"
+    :about="'Big and bigger.'"
+    :description="'From $33.29/mo. for 24 mo. or $799 before trade‑in'"
+    :image="'images/iphone14.jpg'"/>
+
+    <product-screen :title="'iPad Air'"
+    :screen="true"
+    :about="'Light, Bright, Full of might'"
+    :description="'From $599'"
+    :image="'images/ipadAir.jpg'"
+    />
   </div>
 </template>
 
@@ -28,5 +40,7 @@ data() {
   align-items: center;
   justify-content: center;
   width: 100%;
+  &>*:not(:last-child) {
+  }
 }
 </style>
