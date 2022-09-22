1836f0d2ef5ff6e495db345125a4d6df56159ccf
dmitryload <dmitryload@yahoo.com>
2022-09-22 13:20:31 +0300

    layout for cart

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -27,7 +27,15 @@ export default {}
 .cart {
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  &>* {
+    max-width: 600px;
+    font-size: 20px;
+    line-height: 1.16667;
+    font-weight: 600;
+  }
 
   .cart__title {
     font-size: 40px;
@@ -38,9 +46,36 @@ export default {}
 
   .cart__body {
     display: flex;
+    width: 100%;
+    justify-content: center;
+
+    .cart__product {
+      display: flex;
+      padding-top: 20px;
+      width: 100%;
+      justify-content: space-between;
+      .cart__product_name {
+        max-width: 400px;
+        font-weight: 500;
+        margin-right: 20px;
+      }
+    }
 
     .cart__img {
       height: 150px;
+      margin-right: 10px;
+    }
+  }
+  .cart__total {
+    border-top: 1px solid rgba(141, 141, 141, 0.374);
+    padding-top: 10px;
+    margin-top: 10px;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    .cart__total_name {
+      margin-right: 10px;
+      font-weight: 500;
     }
   }
 }
